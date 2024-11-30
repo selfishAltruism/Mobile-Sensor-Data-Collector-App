eca128b670240d2b445f439898d035c15379aad7
selfishAltruism <officialkyus@gmail.com>
2024-11-30 18:15:13 +0900

    chore: prototype

--- src/utils/Scan.jsx
+++ src/utils/Scan.jsx
@@ -13,51 +13,50 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import RNFS from "react-native-fs";
 
-const INTERVER = 1000;
 const bleManager = new BleManager();
 
 const Scan = () => {
-  const [isScanning, setIsScanning] = useState(false); // 스캔 중 상태
-  const [scanInterval, setScanInterval] = useState(null); // 스캔 간격 관리
+  const [isScanning, setIsScanning] = useState(false);
   const [error, setError] = useState(false);
 
   const magSubscription = useRef(null);
   const gyroSubscription = useRef(null);
 
-  const magCount = useRef(0);
-  const gyroCount = useRef(0);
+  const [cycleClear, setCycleClear] = useState(false);
 
-  // Bluetooth 장치 검색 함수
-  const scanBluetoouth = () => {
-    bleManager.stopDeviceScan();
-    const devices = [];
+  const scanBluetoouth = async () => {
+    return new Promise((resolve, reject) => {
+      const devices = [];
 
-    bleManager.startDeviceScan([], null, (error, device) => {
-      if (error) {
-        console.log("블루투스 검색 중 오류 발생: ", error);
-        setError(true);
-        return;
-      }
+      bleManager.startDeviceScan([], null, (error, device) => {
+        if (error) {
+          console.log("블루투스 검색 중 오류 발생: ", error);
+          reject(error);
+          return;
+        }
 
-      if (device && !devices.some((d) => d.id === device.id)) {
-        devices.push({ id: device.id, name: device.name, rssi: device.rssi });
-      }
+        if (device && !devices.some((d) => d.id === device.id)) {
+          devices.push({ id: device.id, name: device.name, rssi: device.rssi });
+        }
+      });
+
+      setTimeout(async () => {
+        bleManager.stopDeviceScan();
+        console.log("블루투스 스캔 종료");
+
+        try {
+          await AsyncStorage.setItem(
+            Date.now() + "-bluetooth",
+            JSON.stringify(devices)
+          );
+          console.log("블루투스 정보 저장됨");
+          resolve();
+        } catch (error) {
+          console.error("AsyncStorage에 저장 실패:", error);
+          reject(error);
+        }
+      }, 1000); // 5초 후 블루투스 스캔 종료
     });
-
-    setTimeout(async () => {
-      bleManager.stopDeviceScan();
-      console.log("블루투스 스캔 종료");
-
-      try {
-        await AsyncStorage.setItem(
-          Date.now() + "-bluetooth",
-          JSON.stringify(devices)
-        );
-        console.log("블루투스 정보 저장됨");
-      } catch (error) {
-        console.error("AsyncStorage에 저장 실패:", error);
-      }
-    }, INTERVER);
   };
 
   const scanWifi = async () => {
@@ -70,31 +69,75 @@ const Scan = () => {
       console.log("Wi-Fi 스캔 완료");
     } catch (error) {
       console.error("Wi-Fi 스캔 중 오류:", error);
+      throw error;
     }
   };
 
   const startScanning = () => {
     console.log("[시작]");
     setError(false);
-
-    const interval = setInterval(async () => {
-      await scanBluetoouth();
-      await scanWifi();
-    }, INTERVER);
-
-    setScanInterval(interval);
     setIsScanning(true);
+
+    // 블루투스와 Wi-Fi 스캔을 모두 끝낼 때까지 기다림
+    Promise.all([scanBluetoouth(), scanWifi()])
+      .then(() => {
+        console.log("모든 스캔 완료");
+        setCycleClear(true);
+      })
+      .catch((error) => {
+        console.log("스캔 중 오류 발생: ", error);
+        setError(true);
+        setIsScanning(false);
+      });
   };
 
   const stopScanning = () => {
     console.log("[종료]");
-    clearInterval(scanInterval);
-    bleManager.stopDeviceScan();
     setIsScanning(false);
+    bleManager.stopDeviceScan();
 
     if (gyroSubscription.current) gyroSubscription.current.unsubscribe();
     if (magSubscription.current) magSubscription.current.unsubscribe();
   };
+
+  useEffect(() => {
+    if (isScanning && cycleClear) {
+      setCycleClear(false);
+      startScanning();
+    }
+  }, [cycleClear]);
+
+  /* useEffect(() => {
+    if (isScanning) {
+      if (magSubscription.current) magSubscription.current.unsubscribe();
+      setUpdateIntervalForType(SensorTypes.magnetometer, 1000);
+      magSubscription.current = magnetometer.subscribe(
+        (data) => {
+          AsyncStorage.setItem(Date.now() + "-magnet", JSON.stringify(data));
+          console.log("마그네토미터 측정 완료");
+        },
+        (error) => console.error("마그네토미터 오류: ", error)
+      );
+    } else {
+      if (magSubscription.current) magSubscription.current.unsubscribe();
+    }
+  }, [isScanning]);
+
+  useEffect(() => {
+    if (isScanning) {
+      if (gyroSubscription.current) gyroSubscription.current.unsubscribe();
+      setUpdateIntervalForType(SensorTypes.gyroscope, 1000);
+      gyroSubscription.current = gyroscope.subscribe(
+        (data) => {
+          AsyncStorage.setItem(Date.now() + "-gyroscope", JSON.stringify(data));
+          console.log("자이로 측정 완료");
+        },
+        (error) => console.error("자이로 오류: ", error)
+      );
+    } else {
+      if (gyroSubscription.current) gyroSubscription.current.unsubscribe();
+    }
+  }, [isScanning]); */
 
   const saveDataToFile = async () => {
     try {
@@ -113,47 +156,6 @@ const Scan = () => {
       console.error("데이터 저장 중 오류:", error);
     }
   };
-
-  useEffect(() => {
-    if (isScanning) {
-      if (magSubscription.current) magSubscription.current.unsubscribe();
-      setUpdateIntervalForType(SensorTypes.magnetometer, INTERVER);
-      magSubscription.current = magnetometer.subscribe(
-        (data) => {
-          if (magCount.current > 1) {
-            magCount.current = 0;
-            return;
-          }
-          magCount.current++;
-          AsyncStorage.setItem(Date.now() + "-magnet", JSON.stringify(data));
-          console.log("마그네토미터 측정 완료");
-        },
-        (error) => console.error("마그네토미터 오류: ", error)
-      );
-    } else {
-      if (magSubscription.current) magSubscription.current.unsubscribe();
-    }
-  }, [isScanning]);
-
-  useEffect(() => {
-    if (isScanning) {
-      if (gyroSubscription.current) gyroSubscription.current.unsubscribe();
-      setUpdateIntervalForType(SensorTypes.gyroscope, INTERVER);
-      gyroSubscription.current = gyroscope.subscribe(
-        (data) => {
-          if (gyroCount.current > 1) {
-            gyroCount.current = 0;
-            return;
-          }
-          AsyncStorage.setItem(Date.now() + "-gyroscope", JSON.stringify(data));
-          console.log("자이로 측정 완료");
-        },
-        (error) => console.error("자이로 오류: ", error)
-      );
-    } else {
-      if (gyroSubscription.current) gyroSubscription.current.unsubscribe();
-    }
-  }, [isScanning]);
 
   const styles = StyleSheet.create({
     button: {
@@ -184,9 +186,7 @@ const Scan = () => {
   return (
     <>
       {error ? (
-        <Text style={styles.error}>
-          블루투스 에러가 발생하여, 재가동 되었습니다.
-        </Text>
+        <Text style={styles.error}>블루투스 에러가 발생하였습니다.</Text>
       ) : null}
       <View>
         <TouchableOpacity
